d449c7670ca1030f09ded078a1e4f6da76d940b8
Vach23 <waseknemec@gmail.com>
2023-11-27 17:35:07 +0100

    Added lib

---
+++ lib/stepper_stm32.c
@@ -0,0 +1,17 @@
+#include "stepper_stm32.h"
+
+struct stepper_t {
+    
+};
+
+void stepper_init(*stepper_t) {
+
+}
+
+void stepper_tim_setup(*stepper_t) {
+
+}
+
+void stepper_ISR_calculation(*stepper_t) {
+
+}
---
+++ lib/stepper_stm32.h
@@ -0,0 +1,5 @@
+
+struct stepper_t;
+
+void setup_timer(*stepper_t);
+void ISR_calculation(*stepper_t);
